a617570ba209f22bcc997727d67bf73e706908f7
dr-mimic <gartflores@gmail.com>
2021-09-14 21:33:47 -0600

    adding date-fn and @axe-core to modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import styles from './styles.module.css'
+
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('@axe-core/react')
+  axe(React, ReactDOM, 1000)
+}
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
